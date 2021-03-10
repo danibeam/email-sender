a67ccd4ffda01e0986371ec94de1aadab28b0ad4
Daniel Belmonte <danibeam97@gmail.com>
2021-03-10 21:18:18 +0100

    Solve CORS issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: "http://caktus.eu" }));
 app.use(require("./routes/index"));
 
 app.listen(5002, () => {
