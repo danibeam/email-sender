861d6d5b8136efe8bf102147a3019d8cf2dd7d21
Daniel Belmonte <danibeam97@gmail.com>
2021-09-23 20:14:41 +0200

    CORS for GCPOL

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: "http://caktus.eu" }));
+app.use(cors({ origin: "https://gcpol.es" }));
 app.use(require("./routes/index"));
 
 app.listen(5002, () => {
