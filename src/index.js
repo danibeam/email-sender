6fb363a4d5d2a05eb0bbf5adc47ab703737e4b6e
Daniel Belmonte <danibeam97@gmail.com>
2021-09-24 17:03:56 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -7,35 +7,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  // );
-
-  // Request headers you wish to allow
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-Requested-With,content-type"
-  // );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
 app.use(require("./routes/index"));
 
-app.listen(5002, () => {
-  console.log("HTTP server on port 5002");
-});
+// app.listen(5002, () => {
+//   console.log("HTTP server on port 5002");
+// });
 
 https
   .createServer(
