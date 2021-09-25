d7a900f219e1627de6fb84526574a61b181fddcd
Daniel Belmonte <danibeam97@gmail.com>
2021-09-25 19:34:10 +0200

    Allow CORS

--- src/index.js
+++ src/index.js
@@ -2,29 +2,35 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(require("./routes/index"));
+app.use(
+  cors({
+    origin: ["http://caktus.eu", "https://gcpol.es/", "http://gcpol.es"],
+  })
+);
 
-// app.listen(5002, () => {
-//   console.log("HTTP server on port 5002");
-// });
+app.listen(5002, () => {
+  console.log("HTTP server on port 5002");
+});
 
-https
-  .createServer(
-    {
-      cert: fs.readFileSync(
-        path.resolve(__dirname, "./../certificates/cert.pem")
-      ),
-      key: fs.readFileSync(
-        path.resolve(__dirname, "./../certificates/key.pem")
-      ),
-    },
-    app
-  )
-  .listen("443", () => {
-    console.log("HTTPs server running on port 443");
-  });
+// https
+//   .createServer(
+//     {
+//       cert: fs.readFileSync(
+//         path.resolve(__dirname, "./../certificates/cert.pem")
+//       ),
+//       key: fs.readFileSync(
+//         path.resolve(__dirname, "./../certificates/key.pem")
+//       ),
+//     },
+//     app
+//   )
+//   .listen("443", () => {
+//     console.log("HTTPs server running on port 443");
+//   });
