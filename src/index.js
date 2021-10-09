0b2499101cc872f79d10292b217ed55346f89e43
Daniel Belmonte <danibeam97@gmail.com>
2021-10-09 12:45:54 +0200

    simple cors!

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(require("./routes/index"));
+app.use(cors());
 
 app.listen(5002, () => {
   console.log("server on port 5002");
