601fbe20228dcc179dd836d31ab9b990cfbd8f8f
Daniel Belmonte <danibeam97@gmail.com>
2021-10-09 12:09:59 +0200

    Allow whitelisted domains

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,20 @@ require("dotenv").config();
 const { Router } = require("express");
 const router = Router();
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 
-router.post("/send-email", async (req, res) => {
+const whitelist = ["http://diesan.es", "http://caktus.eu"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+router.post("/send-email", cors(corsOptions), async (req, res) => {
   const { nombre, email, mensaje, dominio, emailDominio } = req.body;
 
   const transporter = nodemailer.createTransport({
