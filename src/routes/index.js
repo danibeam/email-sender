685043909ef3d97db609bde66f53ad83e0daa941
Daniel Belmonte <danibeam97@gmail.com>
2021-10-09 12:44:26 +0200

    preflight

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const whitelist = ["http://diesan.es", "http://caktus.eu"];
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions;
   if (whitelist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: true, preflightContinue: true }; // reflect (enable) the requested origin in the CORS response
   } else {
     corsOptions = { origin: false }; // disable CORS for this request
   }
