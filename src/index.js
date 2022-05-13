8f7a0135ca74a4368091cf29818ba6983e93a1a0
Daniel Belmonte <danibeam97@gmail.com>
2022-05-13 20:21:30 +0200

    Add Netlify Lambda functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const serverless = require("serverless-http");
 
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use(require("./routes/index"));
+app.use("/.netlify/functions/index", require("./routes/index"));
 
-app.listen(5002, () => {
-  console.log("server on port 5002");
-});
+// app.listen(5002, () => {
+//   console.log("server on port 5002");
+// });
+
+module.exports.handler = serverless(app);
